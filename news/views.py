580769ace9d08339da1f607c9fbff9729cb637cb
Nycolas Padilha <763npp@gmail.com>
2024-02-23 13:06:07 -0300

    feat: create news-details-page

--- news/views.py
+++ news/views.py
@@ -7,3 +7,9 @@ def home(request):
     news = News.objects.all()
 
     return render(request, "home.html", {"news": news})
+
+
+def news_details(request, id):
+    news = News.objects.get(pk=id)
+
+    return render(request, "news_details.html", {"news": news})
