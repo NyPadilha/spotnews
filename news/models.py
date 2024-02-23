8eec506b8f4cee71f730b8c0e7cc0655ad887dd4
Nycolas Padilha <763npp@gmail.com>
2024-02-23 11:23:06 -0300

    feat: create Category, User and News models

---
+++ news/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from news.validators import validate_title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    password = models.CharField(max_length=200, null=False, blank=False)
+    role = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, validators=[validate_title])
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to="img/", null=True, blank=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
